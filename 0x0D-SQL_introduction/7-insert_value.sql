05c7511ce8fa9d85071593e37f0216076da54397
Mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-03-26 14:05:29 -0700

    a script that inserts a new row in the table first_table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Insert a new row
--- See above
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
