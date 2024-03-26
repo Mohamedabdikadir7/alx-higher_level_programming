b60d1477535922e2ae033af394af2457f7854dbc
Mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-03-26 13:22:34 -0700

    creating sql commands

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints full description of a table
+-- See above
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Lists all rows of a table
+-- All field should be printed
+SELECT * FROM first_table;
